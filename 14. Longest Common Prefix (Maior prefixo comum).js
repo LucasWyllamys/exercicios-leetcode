83eafaba235b723e9712d70a28f9f0b131e96fc1
Wyllamys <lucas.wyllamys@outlook.com>
2023-05-25 02:22:44 -0300

    24/05/2023

--- 14. Longest Common Prefix (Maior prefixo comum).js
+++ 14. Longest Common Prefix (Maior prefixo comum).js
@@ -5,25 +5,12 @@
  * @return {string}
  */
 
-const strs = ['flor','fluxo','voo']
-const array_strs = strs.splice(',')
+var strs = ["flower","flow","flight"]
 
 var longestCommonPrefix = function(strs) {
-    let char = ''
-    let new_array = []
-    for(let i = 0; i < strs.length; i++) {
-        for(let p = 0; p < strs[i].length; p++) {
-            let char1 = strs[i].charAt(p)
-            let char2 = strs[i+1].charAt(p)
-            if(char1 == char2) {
-                char += char1
-            } else {
-                break
-            }
-        }
-        new_array.push(char)
-    }
-    return new_char
-};
+    let pref = 'fl'
+    let teste = strs[0].indexOf('fl')
+    return teste
+}
 
-console.log(longestCommonPrefix(array_strs))
+console.log(longestCommonPrefix(strs))
